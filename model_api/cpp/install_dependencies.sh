e599577982d74a75b04255b0bdf864a84c0d3f7f
Alexander <alexander.kozlov@intel.com>
2023-02-20 18:45:15 +0400

    Added action pipeline for the c++ sanity test

--- model_api/cpp/install_dependencies.sh
+++ model_api/cpp/install_dependencies.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+#Install OpenCV
+apt-get install libopencv-dev 
+
+# Install OpenVINO
 wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
 
-sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
 
 echo "deb https://apt.repos.intel.com/openvino/2022 focal main" | sudo tee /etc/apt/sources.list.d/intel-openvino-2022.list
 
-sudo apt update
+apt update
 
-sudo apt install openvino
+apt install openvino
