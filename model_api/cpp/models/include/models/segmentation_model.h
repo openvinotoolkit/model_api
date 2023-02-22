e1bb94e6b7664fdfaac3878bc40776811e9f71d7
Alexander <alexander.kozlov@intel.com>
2023-02-22 10:14:45 +0400

    Fixed comment

--- model_api/cpp/models/include/models/segmentation_model.h
+++ model_api/cpp/models/include/models/segmentation_model.h
@@ -26,6 +26,7 @@ class Model;
 }  // namespace ov
 struct InferenceResult;
 struct ResultBase;
+struct ImageResult;
 struct ImageInputData;
 
 #pragma once
@@ -42,7 +43,7 @@ public:
 
     std::unique_ptr<ResultBase> postprocess(InferenceResult& infResult) override;
 
-    virtual std::unique_ptr<ResultBase> infer(const ImageInputData& inputData);
+    virtual std::unique_ptr<ImageResult> infer(const ImageInputData& inputData);
 
 protected:
     void prepareInputsOutputs(std::shared_ptr<ov::Model>& model) override;
--- model_api/cpp/models/src/segmentation_model.cpp
+++ model_api/cpp/models/src/segmentation_model.cpp
@@ -157,7 +157,7 @@ std::unique_ptr<ResultBase> SegmentationModel::postprocess(InferenceResult& infR
     return std::unique_ptr<ResultBase>(result);
 }
 
-std::unique_ptr<ResultBase> SegmentationModel::infer(const ImageInputData& inputData) {
+std::unique_ptr<ImageResult> SegmentationModel::infer(const ImageInputData& inputData) {
     auto result = ModelBase::infer(static_cast<const InputData&>(inputData));
-    return std::unique_ptr<ResultBase>(static_cast<ResultBase*>(result.release()));
+    return std::unique_ptr<ImageResult>(static_cast<ImageResult*>(result.release()));
 }
