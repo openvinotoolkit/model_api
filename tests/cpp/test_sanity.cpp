1afdc9a7002111fc08cc7b99e71552ab13e3fe27
Alexander <alexander.kozlov@intel.com>
2023-02-22 17:58:47 +0400

    Added CLI parameters for tests

--- tests/cpp/test_sanity.cpp
+++ tests/cpp/test_sanity.cpp
@@ -18,13 +18,16 @@
 #include <models/input_data.h>
 #include <models/results.h>
 
+namespace {
+
 using json = nlohmann::json;
 
 std::string PUBLIC_SCOPE_PATH = "../tests/cpp/public_scope.json";
-std::string MODEL_PATH_TEMPLATE = "../tmp/public/%s/FP16/%s.xml";
-std::string IMAGE_PATH = "../tmp/coco128/images/train2017/000000000074.jpg";
+std::string DATA_DIR = "../data";
+std::string MODEL_PATH_TEMPLATE = "public/%s/FP16/%s.xml";
+std::string IMAGE_PATH = "coco128/images/train2017/000000000074.jpg";
 
-struct Test {
+struct TestData {
     std::string name;
     std::string type;
 };
@@ -49,7 +52,7 @@ inline void from_json(const nlohmann::json& j, Test& test)
     test.type = j.at("type").get<std::string>();
 }
  
-std::vector<Test> GetTests(const std::string& path)
+std::vector<TestData> GetTestData(const std::string& path)
 {
     std::ifstream input(path);
     nlohmann::json j;
@@ -59,16 +62,31 @@ std::vector<Test> GetTests(const std::string& path)
  
 TEST_P(ModelParameterizedTest, SynchronousInference)
 {
-    cv::Mat image = cv::imread(IMAGE_PATH);
+    cv::Mat image = cv::imread(DATA_DIR + "/" + IMAGE_PATH);
     if (!image.data) {
         throw std::runtime_error{"Failed to read the image"};
     }
 
     auto model_path = string_format(MODEL_PATH_TEMPLATE, GetParam().name.c_str(), GetParam().name.c_str());
 
-    auto model = DetectionModel::create_model(model_path);
+    auto model = DetectionModel::create_model(DATA_DIR + "/" + model_path);
     auto result = model->infer(image);
     ASSERT_TRUE(result->objects.size() > 0);
 }
+
+} // namespace
  
-INSTANTIATE_TEST_SUITE_P(TestSanityPublic, ModelParameterizedTest, testing::ValuesIn(GetTests(PUBLIC_SCOPE_PATH)));
+INSTANTIATE_TEST_SUITE_P(TestSanityPublic, ModelParameterizedTest, testing::ValuesIn(GetTestData(PUBLIC_SCOPE_PATH)));
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " <path_to_public_scope.json>" << std::endl;
+        return 1;
+    }
+
+    DATA_DIR = argv[1];
+
+    return RUN_ALL_TESTS();
+}
