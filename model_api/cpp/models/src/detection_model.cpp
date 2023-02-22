c36574f7e158ce93c848754294058678af0611ac
Alexander Kozlov <alexander.kozlov@intel.com>
2023-02-22 09:52:56 +0400

    Update model_api/cpp/models/src/detection_model.cpp
    Co-authored-by: Zlobin Vladimir <vladimir.zlobin@intel.com>

--- model_api/cpp/models/src/detection_model.cpp
+++ model_api/cpp/models/src/detection_model.cpp
@@ -129,8 +129,7 @@ std::vector<std::string> DetectionModel::loadLabels(const std::string& labelFile
     return labelsList;
 }
 
-std::unique_ptr<DetectionResult> DetectionModel::infer(const ImageInputData& inputData)
-{
+std::unique_ptr<DetectionResult> DetectionModel::infer(const ImageInputData& inputData) {
     auto result = ModelBase::infer(static_cast<const InputData&>(inputData));
     return std::unique_ptr<DetectionResult>(static_cast<DetectionResult*>(result.release()));
 }
