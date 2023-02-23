fa45b6f3d8c92893d6c01193ed6641e77aca3b4d
Alexander <alexander.kozlov@intel.com>
2023-02-23 11:38:52 +0400

    Fixed style

--- tests/cpp/prepare_data.py
+++ tests/cpp/prepare_data.py
@@ -3,9 +3,16 @@ import json
 import argparse
 
 
-def parepare_model(data_dir="./data", public_scope=Path(__file__).resolve().parent / "public_scope.json"):
-    from openvino.model_api.models import Classification, DetectionModel, SegmentationModel
-    
+def parepare_model(
+    data_dir="./data",
+    public_scope=Path(__file__).resolve().parent / "public_scope.json",
+):
+    from openvino.model_api.models import (
+        Classification,
+        DetectionModel,
+        SegmentationModel,
+    )
+
     with open(public_scope, "r") as f:
         public_scope = json.load(f)
 
@@ -27,14 +34,20 @@ def prepare_data(data_dir="./data"):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Data and model preparate script")
-    parser.add_argument("-d", dest="data_dir",
-                        default="./data",
-                        help="Directory to store downloaded models and datasets")
-    parser.add_argument("-p", dest="public_scope",
-                        default=Path(__file__).resolve().parent / "public_scope.json",
-                        help="JSON file with public model description")
+    parser.add_argument(
+        "-d",
+        dest="data_dir",
+        default="./data",
+        help="Directory to store downloaded models and datasets",
+    )
+    parser.add_argument(
+        "-p",
+        dest="public_scope",
+        default=Path(__file__).resolve().parent / "public_scope.json",
+        help="JSON file with public model description",
+    )
 
     args = parser.parse_args()
-    
+
     parepare_model(args.data_dir, args.public_scope)
     prepare_data(args.data_dir)
