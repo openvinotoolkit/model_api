459ff84cb814afce25a85873283559d68c9b1fc1
Alexander <alexander.kozlov@intel.com>
2023-03-03 16:02:14 +0400

    Style

--- tests/python/accuracy/test_accuracy.py
+++ tests/python/accuracy/test_accuracy.py
@@ -1,3 +1,4 @@
+import json
 import math
 import os
 import sys
@@ -5,14 +6,11 @@ import tempfile
 import time
 from copy import deepcopy
 from pathlib import Path
-import json
 
 import cv2
 import numpy as np
 import pytest
 import requests
-
-
 from openvino.model_api.models import (
     ClassificationModel,
     DetectionModel,
@@ -28,30 +26,35 @@ def process_output(ouput, model_type):
     else:
         raise ValueError("Unknown model type to precess ouput")
 
+
 def read_config(path: Path):
     with open(path, "r") as f:
         return json.load(f)
+
 
 @pytest.fixture(scope="session")
 def data(pytestconfig):
     return pytestconfig.getoption("data")
 
+
 @pytest.fixture(scope="session")
 def dump(pytestconfig):
     return pytestconfig.getoption("dump")
 
+
 @pytest.fixture(scope="session")
 def result(pytestconfig):
     return pytestconfig.test_results
+
 
 @pytest.mark.parametrize(
     ("model_data"), read_config(Path(__file__).resolve().parent / "public_scope.json")
 )
 def test_image_models(data, dump, result, model_data):
     model = eval(model_data["type"]).create_model(model_data["name"], download_dir=data)
-    
+
     test_result = []
-    
+
     if dump:
         result.append(model_data)
         inference_results = []
@@ -64,21 +67,22 @@ def test_image_models(data, dump, result, model_data):
         outputs = model(image)
 
         image_result = []
-        
+
         for i, output in enumerate(outputs):
             output_str = process_output(output, model_data["type"])
             if len(test_data["reference"]) > i:
                 test_result.append(test_data["reference"][i] == output_str)
             else:
                 test_result.append(False)
-            
+
             if dump:
                 image_result.append(output_str)
-                
-        if dump:
-            inference_results.append({"image": test_data["image"], "reference": image_result})
-    if dump:      
-        result[-1]["test_data"] = inference_results
-        
-    assert all(test_result)
 
+        if dump:
+            inference_results.append(
+                {"image": test_data["image"], "reference": image_result}
+            )
+    if dump:
+        result[-1]["test_data"] = inference_results
+
+    assert all(test_result)
