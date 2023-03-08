f0fe0def86a92911f017ed508101d5293f572d97
Zlobin Vladimir <vladimir.zlobin@intel.com>
2023-03-08 18:51:51 +0400

    Align C++ and Python classification accuracy (#30)
    * Align C++ and Python classification accuracy
    
    Replace ASSERT_TRUE with ASSERT_EQ because it reports the actual values if there's mismatch.
    
    Detection accuracy still diverges.
    
    * black format

--- model_api/python/openvino/model_api/models/utils.py
+++ model_api/python/openvino/model_api/models/utils.py
@@ -122,15 +122,11 @@ def load_labels(label_file):
 
 
 def resize_image(image, size, keep_aspect_ratio=False, interpolation=cv2.INTER_LINEAR):
-    if not keep_aspect_ratio:
-        resized_frame = cv2.resize(image, size, interpolation=interpolation)
-    else:
+    if keep_aspect_ratio:
         h, w = image.shape[:2]
         scale = min(size[1] / h, size[0] / w)
-        resized_frame = cv2.resize(
-            image, None, fx=scale, fy=scale, interpolation=interpolation
-        )
-    return resized_frame
+        return cv2.resize(image, None, fx=scale, fy=scale, interpolation=interpolation)
+    return cv2.resize(image, size, interpolation=interpolation)
 
 
 def resize_image_with_aspect(image, size, interpolation=cv2.INTER_LINEAR):
