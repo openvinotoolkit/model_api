3bcf12a064983f4b2340c02042652404d9176161
Wovchena <vladimir.zlobin@intel.com>
2023-04-03 21:24:34 +0400

    Fix older openvino

--- tests/python/precommit/test_save.py
+++ tests/python/precommit/test_save.py
@@ -8,12 +8,10 @@ def test_detector_save(tmp_path):
     embedded_processing = downloaded.get_model().get_rt_info(
         ["model_info", "embedded_processing"]
     )
-    if type(embedded_processing) != str:
+    if type(embedded_processing) != bool:
         # TODO: uncomment after update to 2023.0
         # 2023.0 return OVAny which needs to be casted with astype()
         embedded_processing = embedded_processing.astype(bool)
-    else:
-        embedded_processing = embedded_processing == "YES"
     assert True == embedded_processing
     xml_path = str(tmp_path / "a.xml")
     downloaded.save(xml_path)
@@ -27,12 +25,10 @@ def test_classifier_save(tmp_path):
     downloaded = Model.create_model(
         "efficientnet-b0-pytorch", configuration={"scale_values": [1, 1, 1], "topk": 6}
     )
-    if type(embedded_processing) != str:
+    if type(embedded_processing) != bool:
         # TODO: uncomment after update to 2023.0
         # 2023.0 return OVAny which needs to be casted with astype()
         embedded_processing = embedded_processing.astype(bool)
-    else:
-        embedded_processing = embedded_processing == "YES"
     assert True == embedded_processing
     xml_path = str(tmp_path / "a.xml")
     downloaded.save(xml_path)
@@ -47,12 +43,10 @@ def test_segmentor_save(tmp_path):
         "hrnet-v2-c1-segmentation",
         configuration={"reverse_input_channels": True, "labels": ["first", "second"]},
     )
-    if type(embedded_processing) != str:
+    if type(embedded_processing) != bool:
         # TODO: uncomment after update to 2023.0
         # 2023.0 return OVAny which needs to be casted with astype()
         embedded_processing = embedded_processing.astype(bool)
-    else:
-        embedded_processing = embedded_processing == "YES"
     assert True == embedded_processing
     xml_path = str(tmp_path / "a.xml")
     downloaded.save(xml_path)
