75f9fa722a46336c665bcd7ac4a748233ffad724
Vladislav Sovrasov <vladislav.sovrasov@intel.com>
2023-07-01 08:20:51 +0900

    Add xai for detection tiling

--- model_api/python/openvino/model_api/tilers/detection.py
+++ model_api/python/openvino/model_api/tilers/detection.py
@@ -14,6 +14,7 @@
  limitations under the License.
 """
 
+import cv2 as cv
 import numpy as np
 from openvino.model_api.models.types import NumericalValue
 from openvino.model_api.models.utils import Detection, DetectionResult, nms
@@ -74,6 +75,7 @@ class DetectionTiler(Tiler):
         offset_x, offset_y = meta["coord"][:2]
         detections[:, 2:] += np.tile((offset_x, offset_y), 2)
         output_dict["bboxes"] = detections
+        output_dict["tile_coords"] = meta["coord"]
 
         return output_dict
 
@@ -90,9 +92,15 @@ class DetectionTiler(Tiler):
         """
 
         detections_array = np.empty((0, 6), dtype=np.float32)
+        feature_vectors = []
+        saliency_maps = []
+        tiles_coords = []
         for result in results:
             if len(result["bboxes"]):
                 detections_array = np.concatenate((detections_array, result["bboxes"]))
+            feature_vectors.append(result["features"])
+            saliency_maps.append(result["saliency_map"])
+            tiles_coords.append(result["tile_coords"])
 
         if np.prod(detections_array.shape):
             detections_array, keep_idx = _multiclass_nms(
@@ -100,6 +108,15 @@ class DetectionTiler(Tiler):
             )
             if meta is not None:
                 meta["keep_idx"] = keep_idx
+
+        merged_vector = (
+            np.average(feature_vectors, axis=0) if feature_vectors else np.ndarray(0)
+        )
+        saliency_map = (
+            self._merge_saliency_maps(saliency_maps, shape, tiles_coords)
+            if saliency_maps
+            else np.ndarray(0)
+        )
 
         detected_objects = []
         for i in range(detections_array.shape[0]):
@@ -112,9 +129,92 @@ class DetectionTiler(Tiler):
 
         return DetectionResult(
             detected_objects,
-            np.ndarray(0),
-            np.ndarray(0),
+            saliency_map,
+            merged_vector,
         )
+
+    def _merge_saliency_maps(self, saliency_maps, shape, tiles_coords):
+        """Merged saliency maps from each tile
+
+        Args:
+            saliency_maps: list of saliency maps, shape of each map is (Nc, H, W)
+            shape: shape of the original image
+            tiles_coords: coordinates of tiles
+
+        Returns:
+            Merged saliency map with shape (Nc, H, W)
+        """
+
+        if not saliency_maps:
+            return None
+
+        image_saliency_map = saliency_maps[0]
+
+        if len(image_saliency_map.shape) == 1:
+            return image_saliency_map
+
+        if len(image_saliency_map.shape) == 4:
+            image_saliency_map = image_saliency_map.squeeze(0)
+
+        num_classes = image_saliency_map.shape[0]
+        map_h, map_w = image_saliency_map.shape[1:]
+
+        image_h, image_w, _ = shape
+        ratio = np.array([map_h, map_w]) / self.tile_size
+
+        image_map_h = int(image_h * ratio[0])
+        image_map_w = int(image_w * ratio[1])
+        merged_map = np.zeros((num_classes, image_map_h, image_map_w))
+
+        for i, saliency_map in enumerate(saliency_maps[1:], 1):
+            for class_idx in range(num_classes):
+                if len(saliency_map.shape) == 4:
+                    saliency_map = saliency_map.squeeze(0)
+
+                cls_map = saliency_map[class_idx]
+
+                x_1, y_1, x_2, y_2 = tiles_coords[i]
+                y_1, x_1 = ((y_1, x_1) * ratio).astype(np.uint16)
+                y_2, x_2 = ((y_2, x_2) * ratio).astype(np.uint16)
+
+                map_h, map_w = cls_map.shape
+
+                if (map_h > y_2 - y_1 > 0) and (map_w > x_2 - x_1 > 0):
+                    cls_map = cv.resize(cls_map, (x_2 - x_1, y_2 - y_1))
+
+                map_h, map_w = y_2 - y_1, x_2 - x_1
+
+                for hi, wi in [(h_, w_) for h_ in range(map_h) for w_ in range(map_w)]:
+                    map_pixel = cls_map[hi, wi]
+
+                    if merged_map[class_idx][y_1 + hi, x_1 + wi] != 0:
+                        merged_map[class_idx][y_1 + hi, x_1 + wi] = 0.5 * (
+                            map_pixel + merged_map[class_idx][y_1 + hi, x_1 + wi]
+                        )
+                    else:
+                        merged_map[class_idx][y_1 + hi, x_1 + wi] = map_pixel
+
+        for class_idx in range(num_classes):
+            image_map_cls = image_saliency_map[class_idx]
+            image_map_cls = cv.resize(image_map_cls, (image_map_w, image_map_h))
+
+            merged_map[class_idx] += 0.5 * image_map_cls
+            merged_map[class_idx] = _non_linear_normalization(merged_map[class_idx])
+
+        return merged_map.astype(np.uint8)
+
+
+def _non_linear_normalization(saliency_map: np.ndarray) -> np.ndarray:
+    """Use non-linear normalization y=x**1.5 for 2D saliency maps."""
+
+    min_soft_score = np.min(saliency_map)
+    # make merged_map distribution positive to perform non-linear normalization y=x**1.5
+    saliency_map = (saliency_map - min_soft_score) ** 1.5
+
+    max_soft_score = np.max(saliency_map)
+    saliency_map = 255.0 / (max_soft_score + 1e-12) * saliency_map
+
+    return np.floor(saliency_map)
 
 
 def _multiclass_nms(
