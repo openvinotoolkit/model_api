e4bcf991aed03519d494105d7685203dd45b7ef9
Wovchena <vladimir.zlobin@intel.com>
2023-08-14 14:35:48 +0400

    find_package->find_dependency

--- model_api/cpp/cmake/model_apiConfig.cmake
+++ model_api/cpp/cmake/model_apiConfig.cmake
@@ -1,5 +1,5 @@
 include(CMakeFindDependencyMacro)
-find_package(OpenCV REQUIRED COMPONENTS core imgproc)
-find_package(OpenVINO REQUIRED COMPONENTS Runtime)
+find_dependency(OpenCV REQUIRED COMPONENTS core imgproc)
+find_dependency(OpenVINO REQUIRED COMPONENTS Runtime)
 
 include("${CMAKE_CURRENT_LIST_DIR}/model_apiTargets.cmake")
