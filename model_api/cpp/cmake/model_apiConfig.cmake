06a58f96c04339478aabded842954af77ba5fe11
Wovchena <vladimir.zlobin@intel.com>
2023-08-14 14:27:13 +0400

    Update cmake
    `CMAKE_CXX_STANDARD_REQUIRED`->`CXX_STANDARD_REQUIRED` for target
    
    Align `COMPONENT`s for install
    
    Align package `COMPONENT`s in `model_apiConfig.cmake` with root `CMakeListt.txt`

--- model_api/cpp/cmake/model_apiConfig.cmake
+++ model_api/cpp/cmake/model_apiConfig.cmake
@@ -1,5 +1,5 @@
 include(CMakeFindDependencyMacro)
-find_dependency(OpenCV)
-find_dependency(OpenVINO)
+find_package(OpenCV REQUIRED COMPONENTS core imgproc)
+find_package(OpenVINO REQUIRED COMPONENTS Runtime)
 
 include("${CMAKE_CURRENT_LIST_DIR}/model_apiTargets.cmake")
