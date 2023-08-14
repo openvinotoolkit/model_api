937ecfc8c544a4308f801665895705fde74be464
Wovchena <vladimir.zlobin@intel.com>
2023-08-14 14:40:05 +0400

    Don't set  for find_dependency(), because it inherits  or  from main package

--- model_api/cpp/cmake/model_apiConfig.cmake
+++ model_api/cpp/cmake/model_apiConfig.cmake
@@ -1,5 +1,5 @@
 include(CMakeFindDependencyMacro)
-find_dependency(OpenCV REQUIRED COMPONENTS core imgproc)
-find_dependency(OpenVINO REQUIRED COMPONENTS Runtime)
+find_dependency(OpenCV COMPONENTS core imgproc)
+find_dependency(OpenVINO COMPONENTS Runtime)
 
 include("${CMAKE_CURRENT_LIST_DIR}/model_apiTargets.cmake")
