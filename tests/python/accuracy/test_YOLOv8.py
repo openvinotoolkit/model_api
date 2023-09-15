0d69f3455aed31add3643916e1f51db850f24942
Wovchena <vladimir.zlobin@intel.com>
2023-09-15 14:09:37 +0400

    Path

--- tests/python/accuracy/test_YOLOv8.py
+++ tests/python/accuracy/test_YOLOv8.py
@@ -59,7 +59,7 @@ def _impaths():
 @pytest.mark.parametrize("impath", _impaths())
 @pytest.mark.parametrize("pt", [Path("yolov5mu.pt"), Path("yolov8l.pt")])
 def test_detector(impath, pt):
-    impl_wrapper, ref_wrapper, ref_dir = _cached_models(os.environ["DATA"], pt)
+    impl_wrapper, ref_wrapper, ref_dir = _cached_models(Path(os.environ["DATA"]), pt)
     im = cv2.imread(str(impath))
     assert im is not None
     impl_preds = impl_wrapper(im)
