dc83627587d74b1882ab68b00e76687898f25b29
Wovchena <vladimir.zlobin@intel.com>
2023-09-20 20:49:57 +0400

    Explicit rotated_rect, simplify example

--- examples/cpp/synchronous_api/main.cpp
+++ examples/cpp/synchronous_api/main.cpp
@@ -1,5 +1,5 @@
 /*
-// Copyright (C) 2018-2022 Intel Corporation
+// Copyright (C) 2018-2023 Intel Corporation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -32,41 +32,32 @@
 #include <models/input_data.h>
 #include <models/results.h>
 
-
-int main(int argc, char* argv[]) {
-    try {
-        if (argc != 3) {
-            std::cerr << "Usage : " << argv[0] << " <path_to_model> <path_to_image>"
-                      << std::endl;
-            return EXIT_FAILURE;
-        }
-
-        cv::Mat image = cv::imread(argv[2]);
-        if (!image.data) {
-            throw std::runtime_error{"Failed to read the image"};
-        }
-
-        // Instantiate Object Detection model
-        auto model = DetectionModel::create_model(argv[1]); // works with SSD models. Download it using Python Model API
-
-        // Run the inference
-        auto result = model->infer(image);
-
-        // Process detections
-        for (auto& obj : result->objects) {
-        std::cout << " " << std::left << std::setw(9) << obj.label << " | " << std::setw(10) << obj.confidence
-                    << " | " << std::setw(4) << int(obj.x) << " | " << std::setw(4) << int(obj.y) << " | "
-                    << std::setw(4) << int(obj.x + obj.width) << " | " << std::setw(4) << int(obj.y + obj.height)
-                    << std::endl;
-        }
-
-    } catch (const std::exception& error) {
-        std::cerr << error.what() << std::endl;
-        return 1;
-    } catch (...) {
-        std::cerr << "Unknown/internal exception happened." << std::endl;
-        return 1;
+int main(int argc, char* argv[]) try {
+    if (argc != 3) {
+        throw std::runtime_error(std::string{"Usage: "} + argv[0] + " <path_to_model> <path_to_image>");
     }
 
-    return 0;
+    cv::Mat image = cv::imread(argv[2]);
+    if (!image.data) {
+        throw std::runtime_error{"Failed to read the image"};
+    }
+
+    // Instantiate Object Detection model
+    auto model = DetectionModel::create_model(argv[1]); // works with SSD models. Download it using Python Model API
+
+    // Run the inference
+    auto result = model->infer(image);
+
+    // Process detections
+    for (auto& obj : result->objects) {
+        std::cout << " " << std::left << std::setw(9) << obj.label << " | " << std::setw(10) << obj.confidence
+            << " | " << std::setw(4) << int(obj.x) << " | " << std::setw(4) << int(obj.y) << " | "
+            << std::setw(4) << int(obj.x + obj.width) << " | " << std::setw(4) << int(obj.y + obj.height) << "\n";
+    }
+} catch (const std::exception& error) {
+    std::cerr << error.what() << '\n';
+    return 1;
+} catch (...) {
+    std::cerr << "Non-exception object thrown\n";
+    return 1;
 }
