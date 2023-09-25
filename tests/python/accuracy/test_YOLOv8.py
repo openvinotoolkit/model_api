d8c0e00743e2025f4ef3990f08d9647dab3bbf6c
Wovchena <vladimir.zlobin@intel.com>
2023-09-25 20:35:28 +0400

    Collect metrics

--- tests/python/accuracy/test_YOLOv8.py
+++ tests/python/accuracy/test_YOLOv8.py
@@ -7,13 +7,9 @@ import numpy as np
 import openvino.runtime as ov
 import pytest
 from openvino.model_api.models import YOLOv5
-from ultralytics import YOLO
-from ultralytics.models import yolo
 import ultralytics
 import torch
 import types
-
-
 
 
 def _init_predictor(yolo):
@@ -23,10 +19,10 @@ def _init_predictor(yolo):
 @functools.lru_cache(maxsize=1)
 def _cached_models(pt):
     export_dir = Path(
-        YOLO(Path(os.environ["DATA"]) / "ultralytics" / pt, "detect").export(format="openvino", half=True)
+        ultralytics.YOLO(Path(os.environ["DATA"]) / "ultralytics" / pt, "detect").export(format="openvino", half=True)
     )
     impl_wrapper = YOLOv5.create_model(export_dir / (pt.stem + ".xml"), device="CPU")
-    ref_wrapper = YOLO(export_dir, "detect")
+    ref_wrapper = ultralytics.YOLO(export_dir, "detect")
     ref_wrapper.overrides["imgsz"] = (impl_wrapper.w, impl_wrapper.h)
     _init_predictor(ref_wrapper)
     ref_wrapper.predictor.model.ov_compiled_model = ov.Core().compile_model(
@@ -71,14 +67,14 @@ def test_accuracy_detector(impath, pt):
     impl_preds = impl_wrapper(im)
     pred_boxes = np.array(
         [
-            [
+            (
                 impl_pred.xmin,
                 impl_pred.ymin,
                 impl_pred.xmax,
                 impl_pred.ymax,
                 impl_pred.score,
                 impl_pred.id,
-            ]
+            )
             for impl_pred in impl_preds.objects
         ],
         dtype=np.float32,
@@ -98,169 +94,101 @@ def test_accuracy_detector(impath, pt):
         print(impl_preds, end="", file=file)
 
 
-from ultralytics.data.utils import check_cls_dataset, check_det_dataset
-from ultralytics.utils import LOGGER, TQDM, callbacks, colorstr, emojis
-
-torch.inference_mode
-
-class ModelAPIValidator(yolo.detect.DetectionValidator):
-    def __init__(self, args):
-        super().__init__(args=args)
-
-    def __call__(self, model, impl):
-        self.training = False
-        augment = self.args.augment and (not self.training)
-
-        # self.device = model.device  # update device
-        self.args.half = True   # TODO: maybe remove
-        self.args.batch = 1  # TODO: maybe remove
-
-        if isinstance(self.args.data, str) and self.args.data.split('.')[-1] in ('yaml', 'yml'):
-            self.data = check_det_dataset(self.args.data)
-        elif self.args.task == 'classify':
-            self.data = check_cls_dataset(self.args.data, split=self.args.split)
-        else:
-            raise FileNotFoundError(emojis(f"Dataset '{self.args.data}' for task={self.args.task} not found ❌"))
-
-        self.args.workers = 0  # faster CPU val as time dominated by inference, not dataloading  # TODO: maybe remove
-        self.dataloader = self.dataloader or self.get_dataloader(self.data.get(self.args.split), self.args.batch)
-        # self.dataloader.dataset.transforms.transforms = self.dataloader.dataset.transforms.transforms[1:]
-        # self.dataloader.dataset.transforms.transforms = (lambda di: {
-        #     'batch_idx': torch.zeros(len(di['instances'])),
-        #     'bboxes': torch.from_numpy(di['instances'].bboxes),
-        #     'cls': torch.from_numpy(di['cls']),
-        #     'img': torch.from_numpy(di['img'].transpose(2, 0, 1)),
-        #     'im_file': di['im_file'],
-        #     'ori_shape': (640, 640),
-        #     'ratio_pad': [(1.0,), (0.0, 0.0)],
-        # },)
-        class asdf:
-            def __init__(self):
-                self.names = {idx: label for idx, label in enumerate(impl.labels)}
-        # model.names = {idx: label for idx, label in enumerate(impl.labels)}  # TODO: meayby empty dict()
-        self.init_metrics(asdf())
-        self.jdict = []  # empty before each val
-        for batch_i, batch in enumerate(self.dataloader):
-            self.run_callbacks('on_val_batch_start')
-            self.batch_i = batch_i
-            # im = batch['img'][0].numpy().transpose(1, 2, 0)
-            im = cv2.imread(*batch['im_file'])
-            # batch['img'] = torch.from_numpy(im.transpose(2, 0, 1)[None])
-            pred = model.predict(im, conf=0.001)
-
-            # self.orig_width = 640
-            # self.orig_height = 640
-
-            # scale = min(self.orig_width / im.shape[1], self.orig_height / im.shape[0])
-            # preds = torch.tensor([(obj.xmin, obj.ymin, obj.xmax, obj.ymax, obj.score, obj.id) for obj in pred.objects], dtype=torch.float32)
-            preds = pred[0].boxes.data
-            # batch['bboxes'] = batch['bboxes'] * torch.tensor((640, 640, 640, 640))
-            # batch['ori_shape'] = [(640, 640),]
-            # img1_shape = 640, 640
-            # img0_shape = im.shape[1], im.shape[0]
-            # gain = min(img1_shape[0] / img0_shape[0], img1_shape[1] / img0_shape[1])  # gain  = old / new
-            # pad = round((img1_shape[1] - img0_shape[1] * gain) / 2 - 0.1), round(
-            #     (img1_shape[0] - img0_shape[0] * gain) / 2 - 0.1)  # wh padding
-            preds[:, :4] *= batch['ratio_pad'][0][0][0]
-            preds[:, :4] += torch.tensor([batch['ratio_pad'][0][1][0], batch['ratio_pad'][0][1][1], batch['ratio_pad'][0][1][0], batch['ratio_pad'][0][1][1]], dtype=torch.int)
-            # batch['ratio_pad'] = ([batch['ratio_pad'][0], pad],)
-            # batch['ori_shape'] = ([im.shape[1], im.shape[0]],)
-            self.update_metrics([preds], batch)
-            if self.args.plots and batch_i < 3:
-                self.plot_val_samples(batch, batch_i)
-                self.plot_predictions(batch, [preds], batch_i)
-
-            self.run_callbacks('on_val_batch_end')
-        stats = self.get_stats()
-        self.check_stats(stats)
-        self.finalize_metrics()
-        self.print_results()
-        self.run_callbacks('on_val_end')
-        if self.training:
-            model.float()
-            results = {**stats, **trainer.label_loss_items(self.loss.cpu() / len(self.dataloader), prefix='val')}
-            return {k: round(float(v), 5) for k, v in results.items()}  # return results as 5 decimal place floats
-        else:
-            if self.args.save_json and self.jdict:
-                with open(str(self.save_dir / 'predictions.json'), 'w') as f:
-                    LOGGER.info(f'Saving {f.name}...')
-                    # json.dump(self.jdict, f)  # flatten and save
-                stats = self.eval_json(stats)  # update stats
-            if self.args.plots or self.args.save_json:
-                LOGGER.info(f"Results saved to {colorstr('bold', self.save_dir)}")
-            return stats
-
-
-class Format:
-    def __call__(self, labels):
-        """Return formatted image, classes, bounding boxes & keypoints to be used by 'collate_fn'."""
-        img = labels.pop('img')
-        h, w = img.shape[:2]
-        instances = labels['instances']
-        instances.convert_bbox(format='xywh')
-        instances.normalize(w, h)
-        labels['img'] = img
-        return labels
-
-
-import cv2
-class ModelAPIValidator2(yolo.detect.DetectionValidator):
-    def __init__(self, args):
-        super().__init__(args=args)
-
-    def __call__(self, yolo, names):
-        self.data = check_det_dataset(self.args.data)
+class Metrics(ultralytics.models.yolo.detect.DetectionValidator):
+    @torch.inference_mode()
+    def evaluate(self, yolo, names, dataset_yml):
+        # TODO: multilabel, both scales, ceil instead of round
+        self.data = ultralytics.data.utils.check_det_dataset(dataset_yml)
         dataloader = self.get_dataloader(self.data[self.args.split], batch_size=1)
-        dataloader.dataset.transforms.transforms = [dataloader.dataset.transforms.transforms[0], Format(), lambda di: {
+        dataloader.dataset.transforms.transforms = (lambda di: {
             'batch_idx': torch.zeros(len(di['instances'])),
             'bboxes': torch.from_numpy(di['instances'].bboxes),
             'cls': torch.from_numpy(di['cls']),
-            'img': torch.from_numpy(di['img'].transpose(2, 0, 1)),  # [::-1]
+            'img': torch.empty(1, 1, 1),
             'im_file': di['im_file'],
             'ori_shape': di['ori_shape'],
-            'ratio_pad': di['ratio_pad'],
-        }]
+            'ratio_pad': [(1.0, 1.0), (0, 0)],
+        },)
         self.init_metrics(names)
         for batch in dataloader:
-            my_pred = yolo.predict(cv2.imread(*batch['im_file']), conf=0.001)
-            preds = my_pred[0].boxes.data.clone()
-            scale_w, scale_h = batch['ratio_pad'][0][0]
-            pad_w = (640 - round(batch['ori_shape'][0][1] * scale_w)) // 2
-            pad_h = (640 - round(batch['ori_shape'][0][0] * scale_h)) // 2
-            preds[:, (0, 2)] *= scale_w
-            preds[:, (1, 3)] *= scale_h
-            preds[:, (0, 2)] += pad_w
-            preds[:, (1, 3)] += pad_h
-            try:
-                assert (pad_w, pad_h) == batch['ratio_pad'][0][1]
-            except:
-                breakpoint()
-            batch['ratio_pad'] = [([scale_w, scale_h], [pad_w, pad_h])]
-            self.update_metrics([preds], batch)
+            im = cv2.imread(batch['im_file'][0])
+            pred = torch.tensor(
+                [[
+                    (
+                        impl_pred.xmin / im.shape[1],
+                        impl_pred.ymin / im.shape[0],
+                        impl_pred.xmax / im.shape[1],
+                        impl_pred.ymax / im.shape[0],
+                        impl_pred.score,
+                        impl_pred.id,
+                    )
+                    for impl_pred in yolo(im).objects
+                ]],
+                dtype=torch.float32,
+            )
+            if not pred.numel():
+                pred = torch.empty(1, 0, 6)
+            # pred = yolo.predict(im, conf=0.001)[0].boxes.data
+            # pred[:, (0, 2)] /= im.shape[1]
+            # pred[:, (1, 3)] /= im.shape[0]
+            self.update_metrics(pred, batch)
         return self.get_stats()
 
 
-@pytest.mark.parametrize("pt", [Path("yolov8n.pt")])#, Path("yolov5n6u.pt")])
+@pytest.mark.parametrize("pt", [Path("yolov8n.pt")])#[Path("yolov8n.pt")])#, Path("yolov5n6u.pt")])
 def test_detector_metric(pt):
     export_dir = export_dir = Path(
-        YOLO(Path(os.environ["DATA"]) / "ultralytics" / pt, "detect").export(format="openvino", half=True)
+        ultralytics.YOLO(Path(os.environ["DATA"]) / "ultralytics" / pt, "detect").export(format="openvino", half=True)
     )
     impl_wrapper = YOLOv5.create_model(export_dir / pt.with_suffix(".xml"), device="CPU", configuration={"confidence_threshold": 0.001})
 
-    kek = YOLO(export_dir, "detect")
-    kek.overrides["imgsz"] = (impl_wrapper.w, impl_wrapper.h)  # TODO: wh or hw?
+    kek = ultralytics.YOLO(export_dir, "detect")
+    kek.overrides = {"imgsz": (impl_wrapper.w, impl_wrapper.h)}  # TODO: wh or hw?
 
-    mAP50_95 = ModelAPIValidator2(args={"data": "coco128.yaml", "imgsz": impl_wrapper.w})(kek, types.SimpleNamespace(names={idx: label for idx, label in enumerate(impl_wrapper.labels)}))["metrics/mAP50-95(B)"]
+    mAP50_95 = Metrics().evaluate(impl_wrapper, types.SimpleNamespace(names={idx: label for idx, label in enumerate(impl_wrapper.labels)}), "coco8.yaml")["metrics/mAP50-95(B)"]
     print(f"{mAP50_95:.99f}")
-    assert mAP50_95 >= 0.453236284183963278326956469754804857075214385986328125000000000000000000000000000000000000000000000
-    # 0.453149872092887873176181301460019312798976898193359375000000000000000000000000000000000000000000000
-    # AUTO: 0.450826465645132734572086974367266520857810974121093750000000000000000000000000000000000000000000000
-    mAP50_95 = ModelAPIValidator2(args={"data": "coco.yaml", "imgsz": impl_wrapper.w})(kek, types.SimpleNamespace(names={idx: label for idx, label in enumerate(impl_wrapper.labels)}))["metrics/mAP50-95(B)"]
+    assert mAP50_95 >= 0.609230627349896747269042407424421980977058410644531250000000000000000000000000000000000000000000000
+    # pytorch ceil: 0.606196548258608802761671086045680567622184753417968750000000000000000000000000000000000000000000000
+    # vino ceil: 0.609230627349896747269042407424421980977058410644531250000000000000000000000000000000000000000000000
+    # pytorch no ceil: 0.606196548258608802761671086045680567622184753417968750000000000000000000000000000000000000000000000
+    # pytorch no ceil: 0.606196548258608802761671086045680567622184753417968750000000000000000000000000000000000000000000000
+
+    mAP50_95 = Metrics().evaluate(impl_wrapper, types.SimpleNamespace(names={idx: label for idx, label in enumerate(impl_wrapper.labels)}), "coco128.yaml")["metrics/mAP50-95(B)"]
     print(f"{mAP50_95:.99f}")
-    assert mAP50_95 >= 0.370558668805542223978477522905450314283370971679687500000000000000000000000000000000000000000000000
-    # 0.370352469463463451759821509767789393663406372070312500000000000000000000000000000000000000000000000
-    # AUTO: 0.369873965487229283688463965518167242407798767089843750000000000000000000000000000000000000000000000
-    # 0.370148340185503577082215542759513482451438903808593750000000000000000000000000000000000000000000000
+    assert mAP50_95 >= 0.439413760605130543357432770790182985365390777587890625000000000000000000000000000000000000000000000
+    # vino no ceil: 0.454416461188190345943382908444618806242942810058593750000000000000000000000000000000000000000000000
+    # vino ceil: 0.453295803020367371605203743456513620913028717041015625000000000000000000000000000000000000000000000
+    # pytorch ceil: 0.456389257039404527827031188280670903623104095458984375000000000000000000000000000000000000000000000
+    # pytorch no ceil: 0.456607101106700774550972710130736231803894042968750000000000000000000000000000000000000000000000000
+    # pytorch no ceil: 0.456607101106700774550972710130736231803894042968750000000000000000000000000000000000000000000000000
+
+    mAP50_95 = Metrics().evaluate(impl_wrapper, types.SimpleNamespace(names={idx: label for idx, label in enumerate(impl_wrapper.labels)}), "coco.yaml")["metrics/mAP50-95(B)"]
+    print(f"{mAP50_95:.99f}")
+    assert mAP50_95 >= 0.365912774507280713631729440749040804803371429443359375000000000000000000000000000000000000000000000
+    # vino ceil: 0.371225813018740136151052411150885745882987976074218750000000000000000000000000000000000000000000000
+    # veno no ceil: 0.371190303780130126387604150295373983681201934814453125000000000000000000000000000000000000000000000
+    # pytorch ceil: 0.370275365583863536045328146428801119327545166015625000000000000000000000000000000000000000000000000
+    # pytorch no ceil: 0.370294050861252221906738668621983379125595092773437500000000000000000000000000000000000000000000000
+    # pytorch no ceil: 0.370294050861252221906738668621983379125595092773437500000000000000000000000000000000000000000000000
+
+
+    # mAP50_95 = Metrics().evaluate(kek, types.SimpleNamespace(names={idx: label for idx, label in enumerate(impl_wrapper.labels)}), "coco8.yaml")["metrics/mAP50-95(B)"]
+    # print(f"{mAP50_95:.99f}")
+    # assert mAP50_95 >= 0.609230627349896747269042407424421980977058410644531250000000000000000000000000000000000000000000000
+
+    # mAP50_95 = Metrics().evaluate(kek, types.SimpleNamespace(names={idx: label for idx, label in enumerate(impl_wrapper.labels)}), "coco128.yaml")["metrics/mAP50-95(B)"]
+    # print(f"{mAP50_95:.99f}")
+    # assert mAP50_95 >= 0.453236284183963278326956469754804857075214385986328125000000000000000000000000000000000000000000000
+    # # 0.454416461188190345943382908444618806242942810058593750000000000000000000000000000000000000000000000 final
+    # # 0.454480642834196590928996783986804075539112091064453125000000000000000000000000000000000000000000000
+    # # 0.453149872092887873176181301460019312798976898193359375000000000000000000000000000000000000000000000
+    # # AUTO: 0.450826465645132734572086974367266520857810974121093750000000000000000000000000000000000000000000000
+    # mAP50_95 = Metrics().evaluate(kek, types.SimpleNamespace(names={idx: label for idx, label in enumerate(impl_wrapper.labels)}), "coco.yaml")["metrics/mAP50-95(B)"]
+    # print(f"{mAP50_95:.99f}")
+    # assert mAP50_95 >= 0.370558668805542223978477522905450314283370971679687500000000000000000000000000000000000000000000000
+    # 0.371198321535742781218658592479187063872814178466796875000000000000000000000000000000000000000000000
+    # 0.371198321535742781218658592479187063872814178466796875000000000000000000000000000000000000000000000
+    # 0.371190303780130126387604150295373983681201934814453125000000000000000000000000000000000000000000000 final
 
     # mAP50_95 = yolo.detect.DetectionValidator(args={"data": "coco128.yaml", "imgsz": impl_wrapper.w})(kek, asdf(), model=kek.model)["metrics/mAP50-95(B)"]
     # print(f"{mAP50_95:.99f}")
