7c1c62df56b1045accd9937fe47ea11a44d806fe
Ashwin Vaidya <ashwinitinvaidya@gmail.com>
2023-12-12 16:59:46 +0100

    return anomaly maps in range 0-255

--- model_api/python/openvino/model_api/models/anomaly.py
+++ model_api/python/openvino/model_api/models/anomaly.py
@@ -70,6 +70,8 @@ class AnomalyDetection(ImageModel):
         assert anomaly_map is not None
         pred_mask = (anomaly_map >= self.pixel_threshold).astype(np.uint8)
         anomaly_map = self._normalize(anomaly_map, self.pixel_threshold)
+        anomaly_map *= 255
+        anomaly_map = anomaly_map.astype(np.uint8)
         pred_mask = cv2.resize(
             pred_mask, (meta["original_shape"][1], meta["original_shape"][0])
         )
