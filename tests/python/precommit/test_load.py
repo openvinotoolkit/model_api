89245fd24df037eb0a3b04d8224bb74e87cccd1c
Vladislav Sovrasov <vladislav.sovrasov@intel.com>
2023-12-21 09:14:35 +0900

    Update load test

--- tests/python/precommit/test_load.py
+++ tests/python/precommit/test_load.py
@@ -1,8 +1,8 @@
 from openvino.model_api.models import Model
 
 
-def test_detector_save():
+def test_model_with_unnamed_output_load():
     # the model's output doesn't have a name
     _ = Model.create_model(
-        "data/otx_models/tinynet_test.xml", model_type="Classification", preload=True
+        "data/otx_models/tinynet_imagenet.xml", model_type="Classification", preload=True
     )
