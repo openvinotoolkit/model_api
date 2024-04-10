96c6c1487e58c6aa8d598df288761c7321e493fa
Vladislav Sovrasov <vladislav.sovrasov@intel.com>
2024-04-11 07:50:14 +0900

    Update version

--- model_api/python/setup.py
+++ model_api/python/setup.py
@@ -23,7 +23,7 @@ SETUP_DIR = Path(__file__).resolve().parent
 
 setup(
     name="openvino_model_api",
-    version="0.1.9",
+    version="0.1.9.1",
     description="Model API: model wrappers and pipelines for inference with OpenVINO",
     author="Intel(R) Corporation",
     url="https://github.com/openvinotoolkit/model_api",
