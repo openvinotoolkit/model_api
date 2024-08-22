63d19843c8d8329d844e63c59caf72659fcef150
Eugene Liu <eugene.liu@intel.com>
2024-08-22 11:29:47 +0100

    reformat

--- model_api/python/model_api/models/instance_segmentation.py
+++ model_api/python/model_api/models/instance_segmentation.py
@@ -195,15 +195,17 @@ class MaskRCNNModel(ImageModel):
             saliency_maps = [[] for _ in range(len(self.labels))]
         else:
             saliency_maps = []
-        for box, confidence, cls, raw_mask in zip(
-            boxes, scores, labels, masks
-        ):
+        for box, confidence, cls, raw_mask in zip(boxes, scores, labels, masks):
             x1, y1, x2, y2 = box
             if (x2 - x1) * (y2 - y1) < 1 or (
                 confidence <= self.confidence_threshold and not has_feature_vector_name
             ):
                 continue
-            str_label = self.labels[cls] if self.labels and cls < len(self.labels) else f"#{cls}"
+            str_label = (
+                self.labels[cls]
+                if self.labels and cls < len(self.labels)
+                else f"#{cls}"
+            )
             raw_cls_mask = raw_mask[cls, ...] if self.is_segmentoly else raw_mask
             if self.postprocess_semantic_masks or has_feature_vector_name:
                 resized_mask = _segm_postprocess(
