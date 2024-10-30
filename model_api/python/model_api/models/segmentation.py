4a3593011f23dcbf38c110ad1ace2fe102c6dd14
Ashwin Vaidya <ashwinnitinvaidya@gmail.com>
2024-10-30 14:31:11 +0100

    add annotations
    Signed-off-by: Ashwin Vaidya <ashwinnitinvaidya@gmail.com>

--- model_api/python/model_api/models/segmentation.py
+++ model_api/python/model_api/models/segmentation.py
@@ -13,6 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+from __future__ import annotations  # TODO: remove when Python3.9 support is dropped
+
 from collections.abc import Iterable
 
 import cv2
