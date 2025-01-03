8bb9a5d28c1eb02e75b3c54cf905e3a8e23fc253
Vladisalv Sovrasov <sovrasov.vlad@gmail.com>
2025-01-04 00:21:05 +0900

    Classification bindings without config

--- model_api/cpp/py_bindings/py_models.cpp
+++ model_api/cpp/py_bindings/py_models.cpp
@@ -1,17 +1,21 @@
 #include <nanobind/nanobind.h>
+#include <nanobind/ndarray.h>
 #include <nanobind/operators.h>
+#include <nanobind/stl/map.h>
 #include <nanobind/stl/string.h>
+#include <nanobind/stl/unique_ptr.h>
 #include <nanobind/stl/vector.h>
 
-#include "models/results.h"
+#include <opencv2/core/types.hpp>
+
 #include "models/classification_model.h"
+#include "models/results.h"
 
 namespace nb = nanobind;
 
 NB_MODULE(py_model_api, m) {
     m.doc() = "Nanobind binding for OpenVINO Vision API library";
-    nb::class_<ResultBase>(m, "ResultBase")
-        .def(nb::init<>());
+    nb::class_<ResultBase>(m, "ResultBase").def(nb::init<>());
 
     nb::class_<ClassificationResult::Classification>(m, "Classification")
         .def(nb::init<unsigned int, const std::string, float>())
@@ -24,7 +28,29 @@ NB_MODULE(py_model_api, m) {
         .def_ro("topLabels", &ClassificationResult::topLabels)
         .def("__repr__", &ClassificationResult::operator std::string);
 
-
     nb::class_<ClassificationModel>(m, "ClassificationModel")
-        .def("create_model", nb::overload_cast<const std::string&, const ov::AnyMap&, bool, const std::string&>(&ClassificationModel::create_model));
+        .def_static(
+            "create_model",
+            [](const std::string& model_path,
+               const std::map<std::string, nb::object>& configuration,
+               bool preload,
+               const std::string& device) {
+                return ClassificationModel::create_model(model_path, {}, preload, device);
+            },
+            nb::arg("model_path"),
+            nb::arg("configuration") = ov::AnyMap({}),
+            nb::arg("preload") = true,
+            nb::arg("device") = "AUTO")
+
+        .def("infer", [](ClassificationModel& self, const nb::ndarray<>& input) {
+            if (input.ndim() != 3 || input.shape(2) != 3 || input.dtype() != nb::dtype<uint8_t>()) {
+                throw std::runtime_error("Input image should have HWC_8U layout");
+            }
+
+            int height = input.shape(0);
+            int width = input.shape(1);
+
+            cv::Mat mat_wrapper(height, width, CV_8UC3, input.data());
+            return self.infer(mat_wrapper);
+        });
 }
