a39ef1151cc264beb40e5ad0e0b45158e6b20c4d
Vladisalv Sovrasov <sovrasov.vlad@gmail.com>
2025-01-04 02:25:58 +0900

    Add license

--- model_api/cpp/py_bindings/py_models.cpp
+++ model_api/cpp/py_bindings/py_models.cpp
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2025 Intel Corporation
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include <nanobind/nanobind.h>
 #include <nanobind/ndarray.h>
 #include <nanobind/operators.h>
