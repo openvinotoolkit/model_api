23426c5d31bf952e850d774e558fec806bfead96
Ashwin Vaidya <ashwinnitinvaidya@gmail.com>
2025-01-16 11:48:12 +0100

    Add docstrings
    Signed-off-by: Ashwin Vaidya <ashwinnitinvaidya@gmail.com>

--- src/python/model_api/visualizer/primitive/polygon.py
+++ src/python/model_api/visualizer/primitive/polygon.py
@@ -17,7 +17,21 @@ if TYPE_CHECKING:
 
 
 class Polygon(Primitive):
-    """Polygon primitive."""
+    """Polygon primitive.
+
+    Args:
+        points: List of points.
+        mask: Mask to draw the polygon.
+        color: Color of the polygon.
+
+    Examples:
+        >>> polygon = Polygon(points=[(10, 10), (100, 10), (100, 100), (10, 100)], color="red")
+        >>> polygon = Polygon(mask=mask, color="red")
+        >>> polygon.compute(image).save("polygon.jpg")
+
+        >>> polygon = Polygon(mask=mask, color="red")
+        >>> polygon.compute(image).save("polygon.jpg")
+    """
 
     def __init__(
         self,
@@ -29,6 +43,17 @@ class Polygon(Primitive):
         self.color = color
 
     def _get_points(self, points: list[tuple[int, int]] | None, mask: np.ndarray | None) -> list[tuple[int, int]]:
+        """Get points from either points or mask.
+        Note:
+            Either points or mask should be provided.
+
+        Args:
+            points: List of points.
+            mask: Mask to draw the polygon.
+
+        Returns:
+            List of points.
+        """
         if points is not None and mask is not None:
             msg = "Either points or mask should be provided, not both."
             raise ValueError(msg)
@@ -42,11 +67,27 @@ class Polygon(Primitive):
         return points_
 
     def _get_points_from_mask(self, mask: np.ndarray) -> list[tuple[int, int]]:
+        """Get points from mask.
+
+        Args:
+            mask: Mask to draw the polygon.
+
+        Returns:
+            List of points.
+        """
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         points_ = contours[0].squeeze().tolist()
         return [tuple(point) for point in points_]
 
     def compute(self, image: Image) -> Image:
+        """Compute the polygon.
+
+        Args:
+            image: Image to draw the polygon on.
+
+        Returns:
+            Image with the polygon drawn on it.
+        """
         draw = ImageDraw.Draw(image)
         draw.polygon(self.points, fill=self.color)
         return image
