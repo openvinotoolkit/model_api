6c78d651e15754f633b2e9aca252138dfa6bede0
Mariusz Gumowski <mariusz.gumowski@intel.com>
2025-01-22 13:43:53 +0100

    license header

--- src/cpp/py_bindings/py_keypoint_detection.cpp
+++ src/cpp/py_bindings/py_keypoint_detection.cpp
@@ -1,3 +1,7 @@
+/*
+ * Copyright (C) 2025 Intel Corporation
+ * SPDX-License-Identifier: Apache-2.0
+ */
 #include <nanobind/ndarray.h>
 #include <nanobind/operators.h>
 #include <nanobind/stl/map.h>
