12c7e0db19ad03813b10a10ebba13dc94555526b
Vladisalv Sovrasov <sovrasov.vlad@gmail.com>
2025-01-30 02:46:51 +0900

    Fix linter

--- tests/cpp/accuracy/conftest.py
+++ tests/cpp/accuracy/conftest.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+
 def pytest_addoption(parser):
     parser.addoption("--data", action="store", help="data folder with dataset")
     parser.addoption("--config", action="store", help="path to models config")
