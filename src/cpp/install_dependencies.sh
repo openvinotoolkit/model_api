4f9ed01955895424f84bd5f8ddf62f2d7bea8095
Vladisalv Sovrasov <sovrasov.vlad@gmail.com>
2025-02-07 00:36:21 +0900

    Update OV in cpp tests

--- src/cpp/install_dependencies.sh
+++ src/cpp/install_dependencies.sh
@@ -5,7 +5,7 @@ wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PU
 
 apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
 
-echo "deb https://apt.repos.intel.com/openvino/2024 ubuntu22 main" | sudo tee /etc/apt/sources.list.d/intel-openvino-2024.list
+echo "deb https://apt.repos.intel.com/openvino/2025 ubuntu22 main" | sudo tee /etc/apt/sources.list.d/intel-openvino-2025.list
 
 apt update
 
@@ -13,4 +13,4 @@ apt update
 apt-get install libopencv-dev
 
 # Install OpenVINO
-apt install openvino
+sudo apt install openvino-2025.0.0
