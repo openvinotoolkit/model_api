0fcbb82572f007463c3492a60663e07d71706de7
Vladisalv Sovrasov <sovrasov.vlad@gmail.com>
2025-02-26 01:53:44 +0900

    Fix linter

--- tests/python/unit/results/test_det_result.py
+++ tests/python/unit/results/test_det_result.py
@@ -9,7 +9,9 @@ from model_api.models.result import DetectionResult
 
 def test_cls_result():
     tst_vector = np.array([1, 2, 3, 4], dtype=np.float32)
-    det_result = DetectionResult(tst_vector, tst_vector, tst_vector, ["a"], tst_vector, tst_vector)
+    det_result = DetectionResult(
+        tst_vector, tst_vector, tst_vector, ["a"], tst_vector, tst_vector
+    )
 
     assert det_result.labels.dtype == np.int32
     assert len(det_result.label_names) == 1
